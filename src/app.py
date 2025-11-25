f8636f7c9eb1214adf267c0a3dafae01dedd6489
michael-angelok <michaelangelo.karpowicz@gmail.com>
2025-11-25 15:48:25 +0100

    Add endpoint to python app

--- src/app.py
+++ src/app.py
@@ -3,9 +3,22 @@ import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+class Handler(http.server.SimpleHTTPRequestHandler):
 
+    def do_GET(self):
+        if self.path == "/health":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"OK")
+            return
+        else:
+            return super().do_GET()
+            
+            
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
     # Start the server and keep it running until you stop the script
     httpd.serve_forever()
+    
+    
+    
